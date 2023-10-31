671b3aeac47feb4278b8532d30d9bf41425419da
SinaSohrab <sinasohrab84@gmail.com>
2023-10-31 06:46:25 +0330

    Completing the exercise of variables

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -10,6 +10,17 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            double number2 = 12.5;
+            int number = 15;
+            char character = '8';
+            bool yes_or_no = true;
+            const double pi = 3.14;
+            Console.WriteLine(number2);
+            Console.WriteLine(number);
+            Console.WriteLine(yes_or_no);
+            Console.WriteLine(character);
+            Console.WriteLine(pi);
+            Console.ReadKey();
         }
     }
 }
