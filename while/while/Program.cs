e1295297c4daa7639a424076e411dc8832601499
SinaSohrab <sinasohrab84@gmail.com>
2023-11-01 17:28:49 +0330

    emd while project

--- while/while/Program.cs
+++ while/while/Program.cs
@@ -10,6 +10,51 @@ namespace @while
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("stars: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            while (x < number)
+            {
+                Console.Write("*");
+                x++;
+            }
+
+            //=============================================================
+
+
+            int game = 0;
+            do
+            {
+                Console.WriteLine("\n input your number for game: ");
+                game = Convert.ToInt32(Console.ReadLine());
+                if (game == 1)
+                {
+                    Console.WriteLine("game one");
+                }
+                if (game == 2)
+                {
+                    Console.WriteLine("game tow");
+                }
+            }
+            while (game != 0);
+
+            //=========================
+
+            int game2 = 0;
+            while (game2 == 0)
+            {
+                Console.WriteLine("\n input your number for game: ");
+                game2 = Convert.ToInt32(Console.ReadLine());
+                if (game2 == 1)
+                {
+                    Console.WriteLine("game one");
+                }
+                if (game2 == 2)
+                {
+                    Console.WriteLine("game tow");
+                }
+            }
+
         }
     }
 }
