9ad9623488396fc6cf81b5822cd8810d78e5c9b8
SinaSohrab <sinasohrab84@gmail.com>
2023-11-02 12:58:39 +0330

    end Array project

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -10,6 +10,38 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            int[] even_numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                even_numbers[i] = i * 2;
+            }
+
+            for (int i = even_numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(even_numbers[i]);
+            }
+
+            //============================
+
+
+            Console.WriteLine("enter your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int first = 1;
+            int second = 2;
+            int sum = 0;
+            int[] is_iven = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                sum = first + second;
+                first = second;
+                second = sum;
+                is_iven[i] = sum;
+            }
+            Console.WriteLine(sum);
+            for (int i = is_iven.Length -1; i < number; i--)
+            {
+                Console.WriteLine(is_iven[i]);
+            }
         }
     }
 }
