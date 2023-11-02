ef6e2362e3c935b3bb0185082cd1f2331068eb05
SinaSohrab <sinasohrab84@gmail.com>
2023-11-02 12:24:09 +0330

    end for_loops project

--- For_Loops/For_Loops/Program.cs
+++ For_Loops/For_Loops/Program.cs
@@ -10,7 +10,40 @@ namespace For_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hio");
+            Console.WriteLine("please insert your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+
+            //==============================
+            // or
+
+            int sum2 = 0;
+
+            for (int i = 2; i <= number; i += 2)
+            {
+                sum2 += i;
+            }
+            Console.WriteLine(sum2);
+
+            // fibonacci number
+            int first = 1;
+            int second = 1;
+            int sum3 = 0;
+            for (int i = 0; i < number; i++)
+            {
+                sum3 = first + second;
+                first = second;
+                second = sum3;
+            }
+            Console.WriteLine(sum3);
         }
     }
 }
