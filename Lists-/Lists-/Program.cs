74b6fee2b3118afc936b6530da679e1e2bb592df
SinaSohrab <sinasohrab84@gmail.com>
2023-11-03 14:09:58 +0330

    end list project

--- Lists-/Lists-/Program.cs
+++ Lists-/Lists-/Program.cs
@@ -10,6 +10,26 @@ namespace Lists_
     {
         static void Main(string[] args)
         {
+            List<int> number = new List<int>();
+            for (int i = 0; i <= 5; i++)
+            {
+                number.Add(i);
+            }
+            number.Insert(2, 50);
+            number.RemoveAt(4);
+            number.Remove(5);
+
+            bool check = number.Contains(50);
+            int count = number.Count;
+            int idx = number.IndexOf(50);
+            number[0] = 11;
+            foreach (int i in number)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(check);
+            Console.WriteLine(count);
+            Console.WriteLine(idx);
         }
     }
 }
