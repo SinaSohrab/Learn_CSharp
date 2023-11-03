04ceed109be8bddc8dcba6918574288174a51c30
SinaSohrab <sinasohrab84@gmail.com>
2023-11-03 15:15:41 +0330

    end methodes project

--- Methodes/Methodes/Program.cs
+++ Methodes/Methodes/Program.cs
@@ -8,8 +8,29 @@ namespace Methodes
 {
     internal class Program
     {
+        public static double calculate_average(List<int> user_ranks)
+        {
+            double sum = 0;
+            for (int i = 0; i < user_ranks.Count; i++)
+            {
+                sum += user_ranks[i];
+            }
+            double average = sum / user_ranks.Count;
+            return average;
+
+        }
         static void Main(string[] args)
         {
+            string[] games = new string[3] { "red dead", "dota2", "gta" };
+            List<int> ranks = new List<int>();
+            for (int i = 0; i < games.Length; i++)
+            {
+                Console.WriteLine("pleas insert your rank for the game " + games[i] + ":");
+                int rank = Convert.ToInt32(Console.ReadLine());
+                ranks.Add(rank);
+            }
+            double average = calculate_average(ranks);
+            Console.WriteLine("avrage rank is: " + average);
         }
     }
 }
